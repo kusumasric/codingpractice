7acab5dc3958754cf4bfac4bd49b96fb49bc0052
kusumasric <kusuma.chunduru@gmail.com>
2016-05-25 12:36:00 -0700

    changed file node.java

--- misc/node.java
+++ misc/node.java
@@ -11,18 +11,51 @@ public class node
 		data=d;
 		next=null;
 	}
-
-public static void main(String[] args)
-{
-	int d1;
-	System.out.println("Enter data\n");
-	d1=Integer.parseInt(System.console().readLine());
-	node current=new node(d1);
-	node th=current;
-	while(th.next!= null)
+	public static node th;
+	public  static node head;
+	public static void main(String[] args)
 	{
-		th=th.next;
+		String str;
+		int d1,count=0;		
+		System.out.println("Enter the input or for exit type 'exit' \n");
+		str=System.console().readLine();
+		
+		while( str.equals("exit")!= true);
+		{
+			
+			d1=Integer.parseInt(str);
+			node current=new node(d1);
+			if(count==0)
+			{
+				head=current;
+				count++;
+			}	
+		
+			th=head;
+			while(th.next!= null)
+			{
+				th=th.next;
+			}
+			
+			th.next=current;
+			th.next=null;	
+			str=System.console().readLine();
+		}	
+		
+		else
+		{
+			System.out.println(" enterede exit\n");
+			return;
+		}
+		
+		th=head;
+		while (th.next!=  null)
+		{
+			System.out.println(th.data+"\t");
+		}
+		
+		
 	}
-	th.next=null;	
-}
+	
+	
 }		
